0cf652c53b5fe09cfab8067e05ba9aeecdeeeac4
Divesh <divesh@whydigit.in>
2025-03-19 15:26:42 +0530

    inprogress

--- src/views/Finance/costInvoice/CostRegister.js
+++ src/views/Finance/costInvoice/CostRegister.js
@@ -1,112 +1,144 @@
 import React from 'react';
 import { TextField, Checkbox, FormControlLabel, FormHelperText, FormControl, InputLabel, MenuItem, Select } from '@mui/material';
-import CachedIcon from '@mui/icons-material/Cached';
+import SearchIcon from '@mui/icons-material/Search';
 import ClearIcon from '@mui/icons-material/Clear';
 import ActionButton from 'utils/ActionButton';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import dayjs from 'dayjs';
-import { DatePicker } from '@mui/x-date-pickers';
+import { DateRangePicker } from '@mui/x-date-pickers-pro';
 import { getAllActiveBranches } from 'utils/CommonFunctions';
 import apiCalls from 'apicall';
 import { useEffect, useState } from 'react';
 import { showToast } from 'utils/toast-component';
 import CommonReportTable from 'utils/CommonReportTable';
+import Button from '@mui/material/Button';
+
 function CostRegister() {
   const [orgId, setOrgId] = useState(localStorage.getItem('orgId'));
-  const [isLoading, setIsLoading] = useState(false);
   const [loginUserName, setLoginUserName] = useState(localStorage.getItem('userName'));
+  const [isLoading, setIsLoading] = useState(false);
   const [branchCodeList, setBranchCodeList] = useState([]);
+  const [partyNameList, setPartyNameList] = useState([]);
   const [listView, setListView] = useState(false);
+  const [finYear, setFinYear] = useState(localStorage.getItem('finYear'));
   const [rowData, setRowData] = useState([]);
 
-  const [filters, setFilters] = useState({
+  const [selectedSections, setSelectedSections] = useState({
     date: false,
-    branchCode: false,
+    branch: false,
+    customer: false,
   });
-  // const handleSwitchChange = (event) => {
-  //   setFilters({ ...filters, [event.target.name]: event.target.checked });
-  //   console.log("filter", event.target.checked);
-    
-  // };
-  const handleSwitchChange = (event) => {
+
+  const [visibleSections, setVisibleSections] = useState({
+    date: false,
+    branch: false,
+    customer: false,
+  });
+  const handleCheckboxChange = (event) => {
     const { name, checked } = event.target;
-    setFilters((prevFilters) => ({
-      ...prevFilters,
-      [name]: checked,
+    setSelectedSections((prevState) => ({
+      ...prevState,
+      [name]: checked
     }));
   };
-  
-  const handleSubmit = () => {
-    const params = {};
-    if (filters.startDate) params.startDate = formData.startDate;
-    if (filters.endDate) params.endDate = formData.endDate;
-    if (filters.branchCode) params.branchCode = formData.branchCode;
-
-    console.log("API Call with Params:", params);
-    // Call your API with these params
+  const handleProceed = () => {
+    setVisibleSections({ ...selectedSections });
   };
   const [formData, setFormData] = useState({
     fromDate: null,
     toDate: null,
     branchCode: '',
+    customer: 'All',
+    customerCode:'All'
   });
   const [fieldErrors, setFieldErrors] = useState({
     fromDate: null,
     toDate: null,
     branchCode: '',
+    customerCode:'',
+    branchCode: '',
   });
   const handleClear = () => {
-    setFilters({
+    setVisibleSections({
       date: false,
       branchCode: false,
+      customer: false,
+    });
+    setSelectedSections({
+      date: false,
+      branchCode: false,
+      customer: false,
     });
     setFormData({
-      fromDate: null,
-      toDate: null,
-      branchCode: '',
+      dateRange: [null, null],
+      branchCode: 'All',
+      customer: 'All',
+      customerCode: 'All',
     });
-
     setFieldErrors({
-      fromDate: '',
-      toDate: '',
+      customer: '',
+      customerCode: '',
       branchCode: '',
     });
+    setRowData([]);
   };
+  const handleSelectPartyChange = (e) => {
+    const value = e.target.value;
+    console.log('Selected employeeCode value:', value);
+    const selectedEmp = partyNameList.find((emp) => emp.partyName === value);
+
+    if (selectedEmp) {
+      console.log('Selected party:', selectedEmp);
+      setFormData((prevData) => ({
+        ...prevData,
+        customer: selectedEmp.partyName,
+        customerCode: selectedEmp.partyCode,
+      }));
+    } else {
+      console.log('No party found with the given code:', value);
+    }
+  };
+
   const handleInputChange = (e) => {
     const { name, value, type, selectionStart, selectionEnd } = e.target;
-    const errors={};
-    if(errors) {
-      setFieldErrors((prevErrors) => ({ ...prevErrors, [name]: '' }));
-    }else if (name === 'branchCode') {
-        const selectedBranch = branchCodeList.find((br) => br.branchCode === value);
-        setFormData((prevData) => ({
-          ...prevData,
-          branchCode: selectedBranch.branchCode,
-          // branchCode: selectedBranch ? selectedBranch.branchCode : ''
-        }));
-      } else {
-        let inputValue = value;
-        if (type === 'text' || type === 'textarea') {
-          inputValue = value.toUpperCase();
-        }
-        setFormData((prevData) => ({ ...prevData, [name]: inputValue }));
-
-        setTimeout(() => {
-          const inputElement = document.getElementsByName(name)[0];
-          if (inputElement && inputElement.setSelectionRange) {
-            inputElement.setSelectionRange(selectionStart, selectionEnd);
-          }
-        }, 0);
+  
+    setFieldErrors((prevErrors) => ({
+      ...prevErrors,
+      [name]: '',
+    }));
+  
+    if (name === 'branchCode') {
+      const selectedBranch = branchCodeList.find((br) => br.branchCode === value);
+      setFormData((prevData) => ({
+        ...prevData,
+        branchCode: selectedBranch ? selectedBranch.branchCode : '', // Avoids undefined error
+      }));
+    } else {
+      let inputValue = value;
+      if (type === 'text' || type === 'textarea') {
+        inputValue = value.toUpperCase();
       }
+      setFormData((prevData) => ({ ...prevData, [name]: inputValue }));
+  
+      setTimeout(() => {
+        const inputElement = document.getElementsByName(name)[0];
+        if (inputElement && inputElement.setSelectionRange) {
+          inputElement.setSelectionRange(selectionStart, selectionEnd);
+        }
+      }, 0);
+    }
   };
-  const handleDateChange = (field, date) => {
-    const formattedDate = dayjs(date).format('YYYY-MM-DD');
-    setFormData((prevData) => ({ ...prevData, [field]: formattedDate }));
+  const handleDateChange = (newValue) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      dateRange: newValue,
+    }));
   };
-  // useEffect(() => {
-  //   getAllBranches();
-  // }, []);
+  useEffect(() => {
+    getAllBranches();
+    getPartyName();
+  }, []);
 
   const getAllBranches = async () => {
     try {
@@ -114,6 +146,14 @@ function CostRegister() {
       setBranchCodeList(branchData);
     } catch (error) {
       console.error('Error fetching country data:', error);
+    }
+  };
+  const getPartyName = async () => {
+    try {
+      const response = await apiCalls('get', `/taxInvoice/getPartyNameByPartyType?orgId=${orgId}&partyType=customer`);
+      setPartyNameList(response.paramObjectsMap.partyMasterVO);
+    } catch (error) {
+      console.error('Error fetching gate passes:', error);
     }
   };
   const reportColumns = [
@@ -145,6 +185,7 @@ function CostRegister() {
     // }
     const saveFormData = {
       branchCode: formData.branchCode,
+      customer: formData.customerCode,
       fromDate: formData.startDate ? dayjs(formData.startDate).format('YYYY-MM-DD') : null,
       toDate: formData.endDate ? dayjs(formData.endDate).format('YYYY-MM-DD') : null
     };
@@ -153,10 +194,18 @@ function CostRegister() {
     if (Object.keys(errors).length === 0) {
       setIsLoading(true);
       try {
-        const response = await apiCalls(
-          'get',
-          `/payable/getAllPaymentRegister?orgId=${orgId}&fromDate=${saveFormData.fromDate}&toDate=${saveFormData.toDate}&subLedgerName=${saveFormData.subLedgerName}`
-        );
+        let response;
+        if(formData.fromDate && formData.toDate){
+          response = await apiCalls(
+            'get',
+            `/taxInvoice/getReportDetailsForSalesRegister?branchCode=${formData.branchCode}&finyear=${finYear}&fromDate=${formData.fromDate}&orgId=${orgId}&partyCode=${formData.customerCode}&toDate=${formData.toDate}`
+          );
+        }else {
+          response = await apiCalls(
+            'get',
+            `/taxInvoice/getReportDetailsForSalesRegister?branchCode=${formData.branchCode}&finyear=${finYear}&orgId=${orgId}&partyCode=${formData.customerCode}`
+          );
+        }
         if (response.status === true) {
           console.log('Response:', response);
           setRowData(response.paramObjectsMap.PartyMasterVO);
@@ -181,30 +230,65 @@ function CostRegister() {
         <div className="row d-flex ml">
           <div className="d-flex flex-wrap justify-content-start mb-4" style={{ marginBottom: '20px' }}>
             <ActionButton title="Clear" icon={ClearIcon} onClick={handleClear} />
-            <ActionButton title="Go" icon={CachedIcon} isLoading={isLoading} onClick={handleGo} margin="0 10px 0 10px" />
+            <ActionButton title="Search" icon={SearchIcon} isLoading={isLoading} onClick={handleGo} margin="0 10px 0 10px" />
           </div>
         </div>
         <>
             <div className="row">
-              <div className="col-md-2 mb-3">
+              <div className="row">
+              <div className="col-md-2
+               mb-3">
                 <FormControlLabel
-                  control={<Checkbox checked={filters.date} onChange={handleSwitchChange} name="date" color="secondary" />}
+                  control={<Checkbox checked={selectedSections.date} onChange={handleCheckboxChange} name="date" color="secondary" />}
                   label="Date"
                 />
               </div>
               <div className="col-md-2 mb-3">
                 <FormControlLabel
-                  control={<Checkbox checked={filters.branchCode} onChange={handleSwitchChange} name="branchCode" color="secondary" />}
+                  control={<Checkbox checked={selectedSections.customer} onChange={handleCheckboxChange} name="customer" color="secondary" />}
+                  label="Customer"
+                />
+              </div>
+              <div className="col-md-2 mb-3">
+                <FormControlLabel
+                  control={<Checkbox checked={selectedSections.branchCode}  onChange={handleCheckboxChange} name="branchCode" color="secondary" />}
                   label="Branch Code"
                 />
               </div>
-              {filters.date && (
+              <div className="col-md-2 mb-3">
+                <Button
+                  onClick={handleProceed}
+                  color="secondary"
+                  variant="contained"
+                  style={{ textTransform: 'none', padding: '4px 8px', marginTop: '6px' }}
+                  disabled={isLoading}
+                >
+                  Proceed
+                </Button>
+              </div>
+              </div>
+              {/* <Button onClick={() => setShowFields(true)} color="secondary">Proceed</Button> */}
+             
+              {visibleSections.date && (
                 <>
-                  <div className="col-md-3 mb-3">
-                    <FormControl fullWidth variant="filled" size="small">
+                  {/* <div className="col-md-3 mb-3"> */}
+                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                      <div className="col-md-6 mb-3">
+                        <FormControl fullWidth variant="outlined" size="small">
+                          <DateRangePicker
+                            value={formData.dateRange}
+                            onChange={handleDateChange}
+                            format="DD-MM-YYYY"
+                          />
+                        </FormControl>
+                      </div>
+                    </LocalizationProvider>
+                    {/* </div> */}
+                  {/* <div className="col-md-3 mb-3">
+                    {/* <FormControl fullWidth variant="filled" size="small">
                       <LocalizationProvider dateAdapter={AdapterDayjs}>
                         <DatePicker
-                          label="Date of Birth"
+                          label="From Date"
                           value={formData.fromDate ? dayjs(formData.fromDate, 'YYYY-MM-DD') : null}
                           onChange={(date) => handleDateChange('fromDate', date)}
                           slotProps={{
@@ -212,14 +296,15 @@ function CostRegister() {
                           }}
                           format="DD-MM-YYYY"
                         />
+                        </DemoItem>
                       </LocalizationProvider>
                     </FormControl>
                   </div>
                   <div className="col-md-3 mb-3">
-                    <FormControl fullWidth variant="filled" size="small">
-                      <LocalizationProvider dateAdapter={AdapterDayjs}>
-                        <DatePicker
-                          label="Date of Join"
+                     <FormControl fullWidth variant="filled" size="small">
+                      <LocalizationProvider dateAdapter={AdapterDayjs}> 
+                         <DatePicker 
+                          label="To Date"
                           value={formData.toDate ? dayjs(formData.toDate, 'YYYY-MM-DD') : null}
                           onChange={(date) => handleDateChange('toDate', date)}
                           slotProps={{
@@ -227,12 +312,13 @@ function CostRegister() {
                           }}
                           format="DD-MM-YYYY"
                         />
-                      </LocalizationProvider>
-                    </FormControl>
-                  </div>
+                        </DemoItem>
+                       </LocalizationProvider>
+                    </FormControl> 
+                  </div> */}
                 </>
               )}
-              {filters.branchCode && (
+              {visibleSections.branchCode && ( 
               <div className="col-md-3 mb-3">
                 <FormControl size="small" variant="outlined" fullWidth error={!!fieldErrors.branchCode}>
                   <InputLabel id="branchCode-label">Branch Code</InputLabel>
@@ -243,6 +329,8 @@ function CostRegister() {
                     onChange={handleInputChange}
                     name="branchCode"
                   >
+                    <MenuItem value="All">All</MenuItem>
+
                     {branchCodeList?.map((row) => (
                       <MenuItem key={row.id} value={row.branchCode}>
                         {row.branchCode}
@@ -250,6 +338,29 @@ function CostRegister() {
                     ))}
                   </Select>
                   {fieldErrors.branchCode && <FormHelperText>{fieldErrors.branchCode}</FormHelperText>}
+                </FormControl>
+              </div>
+              )}
+              {visibleSections.customer && ( 
+              <div className="col-md-3 mb-3">
+                <FormControl size="small" variant="outlined" fullWidth error={!!fieldErrors.customer}>
+                  <InputLabel id="customer-label">Customer</InputLabel>
+                  <Select
+                    labelId="customer-label"
+                    label="customer"
+                    value={formData.customer}
+                    onChange={handleSelectPartyChange}
+                    name="customer"
+                  >
+                    <MenuItem value="All">All</MenuItem>
+
+                    {partyNameList?.map((row) => (
+                      <MenuItem key={row.id} value={row.partyName}>
+                        {row.partyName}
+                      </MenuItem>
+                    ))}
+                  </Select>
+                  {fieldErrors.customer && <FormHelperText>{fieldErrors.customer}</FormHelperText>}
                 </FormControl>
               </div>
               )}
