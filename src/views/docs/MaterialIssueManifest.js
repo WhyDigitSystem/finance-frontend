d805614d266ac5ca081133a8e98bd5d69387cb62
VigneshWDS <vignesh@whydigit.in>
2025-03-28 19:11:11 +0530

    Download pdf button removed

--- src/views/docs/MaterialIssueManifest.js
+++ src/views/docs/MaterialIssueManifest.js
@@ -427,7 +427,7 @@ export const MaterialIssueManifest = () => {
                   >
                     Download Consignee Copy
                   </button>
-                  <button
+                  {/* <button
                     className="me-2 bg-blue inline-block rounded bg-primary h-fit px-6 pb-2 pt-2.5 text-xs font-medium leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
                     onClick={() => handleDownloadPdf('Transporter_Copy')}
                     style={{ marginBottom: '20px' }}
@@ -440,7 +440,7 @@ export const MaterialIssueManifest = () => {
                     style={{ marginBottom: '20px' }}
                   >
                     Download Consignor Copy
-                  </button>
+                  </button> */}
                 </div>
                 <div className="">
                   <IoMdClose onClick={() => setOpenDialog(false)} className="cursor-pointer w-8 h-8 mb-3" />
